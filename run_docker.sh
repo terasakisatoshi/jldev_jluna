75f15ce0019172d6235a0f5939b7e531a365da53
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-05-22 01:52:51 +0900

    remove `-i`

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 docker build -t jldev_jluna .
-docker run --rm -it \
+docker run --rm -t \
 	-v $PWD:/workspaces -w /workspaces \
 	jldev_jluna /bin/bash -c "cmake -S . -B ./build && cmake --build ./build && ./build/main"
